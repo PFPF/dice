ab065e86bb27bb788ee61c4be1198f3ed80c1199
PF <p_fire@126.com>
2014-11-07 09:20:20 -0500

    New features -- different sides number for each dice

--- dice.py
+++ dice.py
@@ -4,6 +4,7 @@
 from pf import *
 
 numDices = iinp("How many dices would you like to roll?")
-sides = iinp("How many sides on your die?")
+
 for i in range(1, numDices + 1):
-	print "Die {} shows: ".format(i) + str(randint(1, sides))
+	sides = iinp("How many sides on die #{}?".format(i))
+	print "The Die shows: ".format(i) + str(randint(1, sides))
