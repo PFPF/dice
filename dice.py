cbe7920d562824340bfabe05451c4f4e67de9407
PF <p_fire@126.com>
2014-11-07 09:39:11 -0500

    Enter in a new line to make it more beautiful.

--- dice.py
+++ dice.py
@@ -3,10 +3,10 @@
 
 from pf import *
 
-print("This program simulates rolling several dice.\nThe user can choose how many dice are rolled.") # Tell them what the program do
+print("This program simulates rolling several dice.\nThe user can choose how many dice are rolled.\n") # Tell them what the program do
 
-numDices = iinp("How many dices would you like to roll?", True) # positive integer only
-sides = iinp("How many sides on your die?", True) # positive integer only
+numDices = iinp("How many dices would you like to roll?\n", True) # positive integer only
+sides = iinp("How many sides on your die?\n", True) # positive integer only
 
 for i in range(1, numDices + 1):
 	print "Die {} shows: ".format(i) + str(randint(1, sides))
