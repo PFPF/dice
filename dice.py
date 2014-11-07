457bec794caf41ddcb6f2b6a661b76bf9a1dd789
PF <p_fire@126.com>
2014-11-07 09:08:34 -0500

    Ask how many dices

--- dice.py
+++ dice.py
@@ -2,5 +2,8 @@
 # 2 Write a die simulation program
 
 from pf import *
+
+numDices = iinp("How many dices would you like to roll?")
 sides = iinp("How many sides on your die?")
-print "The die shows: " + str(randint(1, sides))
+for i in range(1, numDices + 1):
+	print "Die {} shows: ".format(i) + str(randint(1, sides))
