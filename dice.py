2858c66cb54cd4de04de54ab5de228a050022ed4
PF <p_fire@126.com>
2014-11-07 09:42:53 -0500

    Add the urgents~

--- dice.py
+++ dice.py
@@ -3,8 +3,9 @@
 
 from pf import *
 
+print("This program simulates rolling several dice.\nThe user can choose how many dice are rolled and how many sides of each dice.")
 numDices = iinp("How many dices would you like to roll?")
 
 for i in range(1, numDices + 1):
-	sides = iinp("How many sides on die #{}?".format(i))
+	sides = iinp("How many sides on die #{}?\n".format(i))
 	print "The Die shows: ".format(i) + str(randint(1, sides))
