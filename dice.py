4f1a967d61b24fb0bb2fc5397e77ed1c546ff93b
PF <p_fire@126.com>
2014-11-09 23:32:11 -0500

    There's a mistake. It requires an extra input; haven't prevented the situation with 1 side.

--- dice.py
+++ dice.py
@@ -6,8 +6,10 @@ from pf import *
 print("This program simulates rolling several dice.\nThe user can choose how many dice are rolled.\n") # Tell them what the program do
 
 numDices = iinp("How many dices would you like to roll?\n", True) # positive integer only
-sides = iinp("How many sides on your die?\n", True) # positive integer only
 
 for i in range(1, numDices + 1):
-	sides = iinp("How many sides on die #{}?\n".format(i))
+	while True:
+		sides = iinp("How many sides on die #{}?\n".format(i), True) # positive integer only
+		if(sides == 1): print("There's no object which has 1 side except 'Mobius band'.")
+		else: break
 	print "The Die shows: ".format(i) + str(randint(1, sides))
